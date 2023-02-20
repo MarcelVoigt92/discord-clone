f3df1b71294d29d7a18e701fe5d492408917bf4f
Marcel <Marcel.Voigt92@gmx.de>
2023-02-20 12:47:39 +0100

    dependencies done.

---
+++ discord-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TIME TO SUFFER</h1>
+    </div>
+  );
+}
+
+export default App;
