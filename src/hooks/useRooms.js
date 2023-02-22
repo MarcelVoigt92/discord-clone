668c79773bc4d6a8d8df308d86f6897fa14018c2
Marcel <Marcel.Voigt92@gmx.de>
2023-02-22 16:06:31 +0100

    started to create room component.

---
+++ src/hooks/useRooms.js
@@ -0,0 +1,6 @@
+// Fetch all servers
+// react-router by click update url und übergib id von server
+// mit useParams die ID von server rausholen und weiterleiten an einen neuen hook (useRooms) der als parameter die ID von rooms hat der angelickt wird
+// react-router by click update url und übergib id von rooms (localhost:3000/server/id/rooms/roomid)
+// finally show messages belonging to selected room.
+// FINALFINALLY BOOM HEADSHOT
