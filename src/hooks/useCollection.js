6f1bf1cb04e14788200280e7f711df2177218633
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-02-22 14:28:53 +0100

    reduc still not done

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,32 @@
+import { useState, useRef, useEffect } from "react";
+import { db } from "../firebase/config";
+
+export const useCollection = (_collection, _query) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+  const query = useRef(_query).current;
+
+  useEffect(() => {
+    let ref = db.collection(_collection);
+    if (query) {
+      ref = ref.where(...query);
+    }
+
+    const unsub = ref.onSnapshot(
+      (snapShot) => {
+        let result = [];
+        snapShot.docs.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        });
+        setDocument(result);
+        setError(null);
+      },
+      (err) => {
+        setError("Could not find the Document");
+      }
+    );
+
+    return () => unsub;
+  }, [_collection, query]);
+  return { document, error };
+};
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,9 +1,44 @@
 import * as actions from "../constants/userConstant";
+import { db, auth } from "../../firebase/config";
+import { useCollection } from "../../hooks/useCollection";
+import { useState } from "react";
+import { async } from "@firebase/util";
 
 export const login = (email, password) => async (dispatch) => {
+  const [error, setError] = useState("");
   try {
     dispatch({
       type: actions.USER_LOGIN_REQUEST,
     });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const data = await auth.signInWithEmailAndPassword(email, password);
+
+    await db.collection("users").doc(data.user.uid).update({
+      onlineStatus: true,
+    });
+
+    dispatch({
+      type: actions.USER_LOGIN_SUCCESS,
+      payload: data.user,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: actions.USER_LOGIN_FAIL,
+    });
+  }
+};
+
+export const logout = async () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  try {
+    const { uid } = projectAuth.currentUser;
   } catch (error) {}
+  dispatch({ type: actions.USER_LOGOUT });
 };
