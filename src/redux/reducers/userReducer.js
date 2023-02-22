d9aaf4e956dff51d99252f5d9feb06eff76e247e
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-02-22 14:28:53 +0100

    reduc still not done

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,3 +1,10 @@
 import * as actions from "../constants/userConstant";
 
-export const userReducer = () => {};
+export const userLogInReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actions.USER_LOGIN_REQUEST:
+      return;
+    case actions.USER_LOGIN_SUCCESS:
+      return { user: action.payload };
+  }
+};
