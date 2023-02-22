4c3db728ef6147a2b2200d1aaf0e0e1c5e6d982b
dkathrine <101878099+dkathrine@users.noreply.github.com>
2023-02-22 11:49:57 +0100

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, SidebarServer, SidebarUsers, HeaderChat, HeaderServer, Footer } from "./components/index";
+import { Navbar, SidebarServer, SidebarUsers, HeaderChat, HeaderServer, Footer } from "./components/index.js";
 
 import "./App.css";
 
