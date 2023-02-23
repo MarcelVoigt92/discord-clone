d92d3f7db031d2b9dba08c1ddb1fff5242727f13
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-02-23 11:07:42 +0100

    login and out hook

---
+++ src/hooks/useLog.js
@@ -0,0 +1,32 @@
+import db from "../firebase/config";
+
+export const useLog = () => {
+  const logInPush = async (user) => {
+    try {
+      await db
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          ...user,
+          online: true,
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logOut = async (user) => {
+    try {
+      await db
+        .collection("users")
+        .doc(user.uid)
+        .update({
+          ...user,
+          online: false,
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return { logInPush, logOut };
+};
--- src/hooks/useLogIn.js
+++
@@ -1,9 +0,0 @@
-// import firebase from "firebase/compat/app";
-// import { auth } from "../firebase/config";
-
-// function useLogin() {
-//   const provider = new firebase.auth.GoogleAuthProvider();
-//   auth.signInWithRedirect(provider);
-// }
-
-// export default useLogin;
