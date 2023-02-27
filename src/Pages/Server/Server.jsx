f8395f9c01191f4f8b0dda656a6468a98f0b9d7d
dkathrine <101878099+dkathrine@users.noreply.github.com>
2023-02-27 19:02:40 +0100

    added .map function to channels(rooms) + online/offline Users

--- src/Pages/Server/Server.jsx
+++ src/Pages/Server/Server.jsx
@@ -14,14 +14,19 @@ import { useEffect, useState } from "react";
 const Server = () => {
   const { id } = useParams();
   const { document, error } = useDocument("servers", id);
+  const [server, setServer] = useState(null);
+  setTimeout(() => {
+    setServer(document);
+  }, 1000);
 
+  console.log("doc", document);
   // const { id } = useParams();
   // console.log(id);
   return (
     <div className="server">
       <HeaderChat />
       <SidebarUsers />
-      <SidebarServer serverName={document?.name} />
+      <SidebarServer serverName={document?.name} rooms={document?.room} />
       <Chat />
       <Input />
     </div>
