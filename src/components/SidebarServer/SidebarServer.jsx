b660a6afcdbe9170ed47938924dcc1334e0fd21b
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-03-04 16:17:19 +0100

    rooms creation and rooms Collection added

--- src/components/SidebarServer/SidebarServer.jsx
+++ src/components/SidebarServer/SidebarServer.jsx
@@ -1,25 +1,54 @@
 // Server Sidebar for accessing and managing Channels
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { AiOutlinePlus } from "react-icons/ai";
 import { HeaderServer } from "../index.js";
 import { GrChannel } from "react-icons/gr";
+
+import CreateRoom from "../CreateRoom/CreateRoom";
 import "./SidebarServer.css";
 import { Footer } from "../index.js";
+import { db } from "../../firebase/config.js";
+import { Link, useParams } from "react-router-dom";
+
 /* , IoIosArrowForward */
 
-function SidebarServer({ serverName, rooms = [] }) {
+function SidebarServer({ serverName }) {
+  const { id } = useParams();
   const [rotateTextArrow, setRotateTextArrow] = useState(false);
   const handleTextRotate = () => setRotateTextArrow(!rotateTextArrow);
   const rotateText = rotateTextArrow ? "rotate(-90deg)" : "rotate(0)";
-
+  const [rooms, setRooms] = useState([]);
   const [rotateVoiceArrow, setRotateVoiceArrow] = useState(false);
   const handleVoiceRotate = () => setRotateVoiceArrow(!rotateVoiceArrow);
   const rotateVoice = rotateVoiceArrow ? "rotate(-90deg)" : "rotate(0)";
+  const [showCreateRomm, setShowCreateRoom] = useState(false);
 
+  const getRooms = () => {
+    const ref = db.collection("servers").doc(id);
+    const rooms = ref.collection("rooms");
+    rooms.onSnapshot((snapShot) => {
+      let result = [];
+      snapShot.docs.forEach((doc) => {
+        result.push({ ...doc.data(), id: doc.id });
+      });
+      setRooms(result);
+    });
+  };
+  useEffect(() => {
+    getRooms();
+  }, [id]);
   return (
     <>
       <div className="sidebar sidebarServer">
+        <>
+          {showCreateRomm && (
+            <CreateRoom
+              showRoom={setShowCreateRoom}
+              show1={setShowCreateRoom}
+            />
+          )}
+        </>
         <HeaderServer serverName={serverName} />
         <div className="sidebar__channels">
           <div className="sidebar__channelsHeader">
@@ -31,14 +60,20 @@ function SidebarServer({ serverName, rooms = [] }) {
               {/* show/hide textchannels */}
               <span onClick={handleTextRotate}>Text Channels</span>
             </div>
-            <AiOutlinePlus className="sidebar__addChannel" />
+            <AiOutlinePlus
+              className="sidebar__addChannel"
+              onClick={() => setShowCreateRoom(!showCreateRomm)}
+            />
           </div>
           <div>
             <ul>
               {rooms.map((room) => (
-                <li key={room.name}>
-                  <GrChannel /> {room.name}
-                </li>
+                <Link to={`rooms/${room.id}`}>
+                  {" "}
+                  <li key={room.id}>
+                    <GrChannel /> {room.roomName}
+                  </li>
+                </Link>
               ))}
             </ul>
           </div>
--- src/pages/Server/Server.jsx
+++ src/pages/Server/Server.jsx
@@ -6,13 +6,16 @@ import {
   Chat,
   Input,
 } from "../../components/index";
+import { db } from "../../firebase/config";
 import { useParams } from "react-router-dom";
 import { useDocument } from "../../hooks/useDocument";
 import { useState } from "react";
+import Room from "../Room/Room";
 
 const Server = () => {
   const { id } = useParams();
   const { document, error } = useDocument("servers", id);
+
   const [server, setServer] = useState(null);
 
   setTimeout(() => {
@@ -25,8 +28,7 @@ const Server = () => {
       <HeaderChat name={server?.room?.[0].name} />
       <SidebarUsers />
       <SidebarServer serverName={server?.name} rooms={server?.room} />
-      <Chat />
-      <Input />
+      <Room />
     </div>
   );
 };
